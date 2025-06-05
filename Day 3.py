85825ce119040e68f353b7ad1f19b103a0a448f0
Creisttiano Raymond <creisttianoraymond@Creisttianos-MacBook-Pro.local>
2025-06-05 21:10:41 +0800

    Finalize Day 3 Task: Loan Eligibity checker

--- Day 3.py
+++ Day 3.py
@@ -1,5 +1,5 @@
 print ("Day 3: Learn to make Python to think and make decisions")
-
+print("Creating eligibility checker for loan")
 
 # print ("Task 1: Trying IF & Else")
 # age = int(input("Enter your age: "))
@@ -11,17 +11,27 @@ print ("Day 3: Learn to make Python to think and make decisions")
 #     print("You are not eligible to vote yet!")
 
 #------------------------------------------------
-print ("Task 2: Trying if, elif & else")
+# print ("Task 2: Trying if, elif & else")
 
-marks = int(input("Enter your marks: "))
-if marks >= 90:
-    print("Excellent score!")
+# marks = int(input("Enter your marks: "))
+# if marks >= 90:
+#     print("Excellent score!")
 
-elif marks >= 80:
-    print("You did well ")
+# elif marks >= 80:
+#     print("You did well ")
 
-elif marks >= 70:
-    print("You did good. You can do better!")
+# elif marks >= 70:
+#     print("You did good. You can do better!")
+
+# else:
+#     print("You need to work hard. You can do it!")
+
+#------------------------------------------------
+age = int(input("Enter your age: "))
+country = input("Enter your country: ")
+
+if age >= 18 and country.lower() == "malaysia":
+    print("You are eligible to to get loan!")
 
 else:
-    print("You need to work hard. You can do it!")
+    print("You are not eligible to get loan yet!")
